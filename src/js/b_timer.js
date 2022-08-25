12b86e73dd9520830cf851a9eedcc8f8052dfd16
Boffeer <41575886+Boffeer@users.noreply.github.com>
2022-08-25 12:17:06 +0300

    Добавляет библиотеки

--- src/js/b_timer.js
+++ src/js/b_timer.js
@@ -2,18 +2,92 @@
  */
 // #region timer
 const timers = document.querySelectorAll(".timer");
-const timerName = "localTimer";
-// const timerDeadline = 48 * 60 * 60 * 1000;
-const timerDeadline = getTimerDeadlineFromNow(0.1);
 
-function getTimerDeadlineFromNow(hours, isDaysInTimer = false) {
-  const milliseconds = 1000;
-  const seconds = 60;
-  const minutes = 60;
-  return hours * minutes * seconds * milliseconds;
-}
-if ([...timers].length > 0) {
-  function getTimeRemaining(deadline) {
+class Timer {
+  timers = [];
+  timerName = "localTimer";
+  timerDeadline = this.getTimerDeadlineFromNow(1);
+
+  constructor() {
+    this.setTimers(".b_timer");
+    this.launchTimers();
+  }
+  setTimers(timerClass) {
+    this.timers = [...document.querySelectorAll(timerClass)];
+  }
+  getTimers() {
+    return this.timers;
+  }
+  launchTimers() {
+    this.isTimerNull() ? this.setDeadline(this.timerDeadline) : false;
+
+    this.getTimers().forEach((timer) => {
+      timer.classList.add("timer--init");
+      this.updateClock();
+      setInterval(this.updateClock.bind(this), 1000);
+      // const timeinterval = setInterval(this.updateClock(timer), 1000);
+    });
+  }
+
+  updateClock() {
+    let { hoursTotal, minutes, seconds, total } = this.getTimeRemaining(
+      this.getDeadline()
+    );
+
+    let left = this.getLeadingZeroTime(hoursTotal);
+    let center = this.getLeadingZeroTime(minutes);
+    let right = this.getLeadingZeroTime(seconds);
+    let timerSpan = [left, center, right];
+
+    this.getTimers().forEach((timer) => {
+      this.getTimerSections(timer).forEach((section, i) => {
+        [...section.querySelectorAll(".timer__digit")].forEach(
+          (digit, index) => {
+            digit.innerText = total >= 0 ? timerSpan[i][index] : "0";
+          }
+        );
+      });
+      if (total <= 0) {
+        // clearInterval(timeinterval);
+        this.onTimerExpire();
+      }
+    });
+  }
+
+  getTimerSections(timer) {
+    let sectionLeft = timer.querySelector(".timer__left");
+    let sectionCenter = timer.querySelector(".timer__center");
+    let sectionRight = timer.querySelector(".timer__right");
+    let timerSections = [sectionLeft, sectionCenter, sectionRight];
+    return timerSections;
+  }
+  getLeadingZeroTime(time) {
+    return ("0" + time).slice(-2);
+  }
+
+  setDeadline(deadline) {
+    const newDeadline = new Date(Date.parse(new Date()) + deadline);
+    localStorage.setItem(this.timerName, newDeadline);
+    return newDeadline;
+  }
+  getDeadline() {
+    return localStorage.getItem(this.timerName);
+  }
+  isTimerNull() {
+    return localStorage.getItem(this.timerName) == null;
+  }
+
+  // var deadline = new Date(Date.parse(new Date()) + 10 * 1000); // for endless timer
+
+  getTimerDeadlineFromNow(hours, isDaysInTimer = false) {
+    const milliseconds = 1000;
+    const seconds = 60;
+    const minutes = 60;
+    return hours * minutes * seconds * milliseconds;
+  }
+  // if ([...timers].length > 0) {
+  getTimeRemaining(deadline) {
+    console.log(deadline);
     let t = Date.parse(deadline) - Date.parse(new Date());
     let seconds = Math.floor((t / 1000) % 60);
     let minutes = Math.floor((t / 1000 / 60) % 60);
@@ -32,62 +106,11 @@ if ([...timers].length > 0) {
     return ticks;
   }
 
-  function onTimerExpire(clock) {
+  onTimerExpire() {
     // clock.style.opacity = "0";
   }
 
-  function initClock(timer, deadline) {
-    // const clock = document.querySelector(timer);
-    const clock = timer;
-    // letk daysSpan = clock.querySelector(".timer-days_value");
-    let hoursSpan = clock.querySelector(".timer__left");
-    let minutesSpan = clock.querySelector(".timer__center");
-    let secondsSpan = clock.querySelector(".timer__right");
-    let time = [hoursSpan, minutesSpan, secondsSpan];
-
-    function getLeadingZeroTime(time) {
-      return ("0" + time).slice(-2);
-    }
-    function updateClock() {
-      let { hoursTotal, minutes, seconds, total } = getTimeRemaining(deadline);
-
-      let left = getLeadingZeroTime(hoursTotal);
-      let center = getLeadingZeroTime(minutes);
-      let right = getLeadingZeroTime(seconds);
-      let timerSpan = [left, center, right];
-
-      time.forEach((span, i) => {
-        [...span.querySelectorAll(".timer__digit")].forEach((digit, index) => {
-          digit.innerText = total >= 0 ? timerSpan[i][index] : "0";
-        });
-      });
-      if (total <= 0) {
-        clearInterval(timeinterval);
-        onTimerExpire(clock);
-      }
-    }
-
-    updateClock();
-    var timeinterval = setInterval(updateClock, 1000);
-  }
-  function setTimer(deadline) {
-    localStorage.setItem(
-      timerName,
-      new Date(Date.parse(new Date()) + deadline)
-    );
-    return getTimer();
-  }
-  function getTimer() {
-    return localStorage.getItem(timerName);
-  }
-  function isTimerNull() {
-    return localStorage.getItem(timerName) == null;
-  }
-  const deadline = isTimerNull() ? setTimer(timerDeadline) : getTimer();
-  // var deadline = new Date(Date.parse(new Date()) + 10 * 1000); // for endless timer
-
-  timers.forEach((timer) => {
-    initClock(timer, deadline);
-  });
+  // }
 }
+new Timer();
 // #endregion timer
---
+++ src/js/poppa.js
@@ -0,0 +1,286 @@
+"use strict";
+
+/**
+ * Фиксирует скрол у body
+ *  */
+function bodyLock(con) {
+  let scrollFix = window.innerWidth - document.body.clientWidth;
+  const DEFAULT_SCROLLBAR_WIDTH = 17;
+  if (con === true) {
+    // scrollFix предотвращает скачки верстки в строну при блокировке скролла
+    scrollFix =
+      scrollFix > DEFAULT_SCROLLBAR_WIDTH ? DEFAULT_SCROLLBAR_WIDTH : scrollFix;
+    document.body.style.paddingRight = `${scrollFix}px`;
+    document.body.classList.add("_lock");
+  } else if (con === false) {
+    document.body.classList.remove("_lock");
+  } else if (con === undefined) {
+    if (!document.body.classList.contains("_lock")) {
+      document.body.classList.add("_lock");
+    } else {
+      document.body.classList.remove("_lock");
+    }
+  } else {
+    console.error("Неопределенный аргумент у функции bodyLock()");
+  }
+}
+
+// Открытие модального окна, если в url указан его id
+openModalHash();
+function openModalHash() {
+  if (window.location.hash) {
+    const hash = window.location.hash.substring(1);
+    const modal = document.querySelector(`.modal#${hash}`);
+
+    if (modal) {
+      modal.classList.add("_show");
+      bodyLock(true);
+      closeWhenClickingOnBg(`#${hash} .modal__content`, modal);
+    }
+  }
+}
+
+// Закрытие модальных окон при клике по крестику
+closeModalWhenClickingOnCross();
+function closeModalWhenClickingOnCross() {
+  const modalElems = document.querySelectorAll(".modal");
+  for (let i = 0; i < modalElems.length; i++) {
+    const modal = modalElems[i];
+    const closeThisModal = modal.querySelector(".modal__close");
+
+    closeThisModal.addEventListener("click", () => {
+      modal.classList.remove("_show");
+      bodyLock(false);
+      resetHash();
+    });
+  }
+}
+
+// Сброс id модального окна в url
+function resetHash() {
+  const windowTop = window.pageYOffset;
+  window.location.hash = "";
+  window.scrollTo(0, windowTop);
+}
+
+// Открытие модальных окон
+openModal();
+function openModal() {
+  const btnsOpenModal = document.querySelectorAll("[data-modal-open]");
+  const btnsCloseModal = document.querySelectorAll("[data-modal-close]");
+
+  for (let i = 0; i < btnsOpenModal.length; i++) {
+    const btn = btnsOpenModal[i];
+
+    btn.addEventListener("click", () => {
+      const dataBtn = btn.dataset.modalOpen;
+      const modalThatOpens = document.querySelector(`#${dataBtn}`);
+
+      btn.classList.add("modal-show");
+      modalThatOpens.classList.add("_show");
+      bodyLock(true);
+      closeWhenClickingOnBg(`#${dataBtn} .modal__content`, modalThatOpens);
+      window.location.hash = dataBtn;
+    });
+  }
+
+  btnsCloseModal.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      const dataBtn = btn.dataset.modalClose;
+      const modalThatCloses = document.querySelector(`#${dataBtn}`);
+
+      btn.classList.remove("modal-show");
+      modalThatCloses.classList.remove("_show");
+      bodyLock(false);
+      resetHash();
+    });
+  });
+}
+
+// Закрытие модального окна при клике по заднему фону
+function closeWhenClickingOnBg(itemArray, itemParent, classShow = "_show") {
+  document.addEventListener("click", (e) => {
+    let itemElems = document.querySelectorAll(itemArray);
+
+    for (let i = 0; i < itemElems.length; i++) {
+      const item = itemElems[i];
+
+      const target = e.target,
+        itsItem = target == item || item.contains(target),
+        itemIsShow = item.classList.contains(classShow);
+
+      if (itemParent) {
+        const itsItemParent =
+            target == itemParent || itemParent.contains(target),
+          itemParentIsShow = itemParent.classList.contains(classShow);
+
+        if (!itsItem && itsItemParent && itemParentIsShow) {
+          itemParent.classList.remove(classShow);
+
+          if (document.body.classList.contains("_lock")) {
+            bodyLock(false);
+          }
+
+          if (window.location.hash === "#" + itemParent.getAttribute("id")) {
+            resetHash();
+          }
+        }
+      } else {
+        if (!itsItem && itemIsShow) {
+          item.classList.remove(classShow);
+          if (document.body.classList.contains("_lock")) {
+            bodyLock(false);
+          }
+
+          if (window.location.hash === "#" + itemParent.getAttribute("id")) {
+            resetHash();
+          }
+        }
+      }
+    }
+  });
+}
+
+class Poppa {
+  constructor() {
+    this.initPopups();
+    this.initButtons();
+
+    document.addEventListener("keydown", (e) => {
+      const id = this.getLastOpenedId();
+      if (e.key === "Escape") {
+        console.log(id);
+        this.closePop(id);
+        // modal.classList.remove("_show");
+        // bodyLock(false);
+        // resetHash();
+      }
+    });
+  }
+
+  instances = [];
+  getPopupsStorage() {
+    return document.querySelector(".poppa__storage");
+  }
+
+  initPopups() {
+    const popups = document.querySelectorAll(".poppa");
+    const popupsStorage = document.createElement("div");
+
+    popupsStorage.classList.add("poppa__storage");
+    document.body.append(popupsStorage);
+
+    popups.forEach((poppa) => this.makePopupWrapper(poppa));
+    // TODO: В прототип будет вписываться метод открыть и закрыть
+  }
+
+  makePopupWrapper(poppa) {
+    const id = poppa.id;
+
+    const overlay = document.createElement("div");
+    overlay.classList.add("poppa__overlay");
+    overlay.classList.add(`poppa__overlay--${id}`);
+    overlay.dataset.poppaName = id;
+
+    const aligner = document.createElement("div");
+    aligner.classList.add("poppa__aligner");
+
+    const closer = document.createElement("button");
+    closer.classList.add("poppa__closer");
+    closer.innerText = "Закрыть";
+    closer.dataset.poppaClose = id;
+    const handleClose = () => {
+      this.handleClose(closer);
+    };
+    closer.addEventListener("click", handleClose);
+    // TODO: Сделай накидывание нужных ариа атрибутов
+    // TODO: Сделай накидывание хрефа, который будет закрывать
+
+    document.querySelector(".poppa__storage").append(overlay);
+    aligner.append(poppa);
+    overlay.append(aligner);
+    overlay.append(closer);
+
+    this.instances.push(overlay);
+  }
+
+  initButtons() {
+    const openButtons = document.querySelectorAll("[data-poppa-open]");
+    openButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        this.handleOpen(button);
+      });
+    });
+
+    const closeButtons = document.querySelectorAll("[data-poppa-close]");
+    closeButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        this.handleClose(button);
+      });
+    });
+  }
+
+  handleOpen(button) {
+    const id = button.dataset.poppaOpen;
+    const poppa = document.querySelector(`#${id}`);
+    if (poppa) {
+      this.openPop(id);
+    } else {
+      this.makeInfoPop(`Попапа "#${id}" нет. Проверь правильность айди`);
+    }
+  }
+  openPop(id) {
+    const pop = this.getPop(id);
+    this.getPopupsStorage().append(pop);
+    pop.classList.add("_show");
+  }
+
+  handleClose(button) {
+    const id = button.dataset.poppaClose;
+    const poppa = document.querySelector(`#${id}`);
+    if (poppa) {
+      this.closePop(id);
+    } else {
+      this.makeInfoPop(`Попапа "#${id}" нет. Проверь правильность айди`);
+    }
+  }
+
+  closePop(id) {
+    const pop = this.getPop(id);
+    if (pop.classList.contains("_show")) {
+      pop.classList.remove("_show");
+    }
+  }
+
+  makeInfoPop(text, removeAfter = 6000) {
+    const info = document.createElement("div");
+    const id = "pop-" + new Date().getTime();
+    info.classList.add("poppa");
+    info.classList.add("poppa--invalid");
+    info.innerHTML = text;
+    info.id = id;
+
+    document.body.append(info);
+
+    this.makePopupWrapper(info);
+    this.openPop(id);
+
+    setTimeout(() => {
+      this.getPop(id).remove();
+    }, removeAfter);
+  }
+
+  getPop(id) {
+    return document.querySelector(`[data-poppa-name="${id}"]`);
+  }
+
+  getLastOpenedId() {
+    // let storage = this.getPopupsStorage();
+    // storage = Array.from(storage.children);
+    // return storage[storage.length - 1].dataset.poppaName;
+    const opened = [...document.querySelectorAll("._show[data-poppa-name]")];
+    return opened[opened.length - 1].dataset.poppaName;
+  }
+}
+
+new Poppa();
