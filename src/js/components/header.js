34ae6ea976f29b4457e13180f75a14f97ec92f66
Boffeer <41575886+Boffeer@users.noreply.github.com>
2022-12-21 21:09:11 +0300

    Делает плашку купить

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -291,26 +291,43 @@ stickyHeader();
 /**
  * Липкая «купить» на странице товара в десктопе
  */
-const productSticky = document.querySelector('.product-sticky');
-(()=>{
-  if (!productSticky) return;
 
-  header.addEventListener('header-show', (e) => {
-    header.classList.add('header--no-shadow');
-    productSticky.style.top = header.getBoundingClientRect().height + 'px';
-  });
-  header.addEventListener('header-hide', (e) => {
-    header.classList.remove('header--no-shadow');
-    productSticky.style.top = '';
+window.addEventListener('DOMContentLoaded', (event) => {
+  const productSticky = document.querySelector('.product-sticky');
+
+  function getProductStickyOffset() {
+    let productStickyOffset = productSticky.previousElementSibling.offsetTop + 100
+    
+    return productStickyOffset;
+  }
+
+  let productStickyOffset = getProductStickyOffset();
+  window.addEventListener('resize', () => {
+    productStickyOffset = getProductStickyOffset()
   });
 
-  window.addEventListener("scroll", (e) => {
-    const offset = productSticky.getBoundingClientRect().top
-    if (offset <= productSticky.getBoundingClientRect().height + header.getBoundingClientRect().height) {
-      productSticky.classList.add('product-sticky--visible')
-    } else {
-      productSticky.classList.remove('product-sticky--visible')
-    }
-  });
-})()
+  (() => {
+    if (!productSticky) return;
 
+    header.addEventListener('header-show', (e) => {
+      productSticky.style.top = header.getBoundingClientRect().height + 'px';
+    });
+    header.addEventListener('header-hide', (e) => {
+      productSticky.style.top = '';
+    });
+
+    window.addEventListener("scroll", (e) => {
+      const offset = productSticky.getBoundingClientRect()
+
+      console.log(window.scrollY, productStickyOffset)
+
+      if (window.scrollY <= productStickyOffset) {
+        productSticky.classList.remove('product-sticky--visible');
+        header.classList.remove('header--no-shadow');
+      } else {
+        productSticky.classList.add('product-sticky--visible');
+        header.classList.add('header--no-shadow');
+      }
+    });
+  })()
+});
